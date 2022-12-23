41905ca3c08a919ab328debf0c9e921fef51d597
Raymond Turner <ray@Raymonds-MacBook-Pro.local>
2022-12-23 01:23:07 +0000

    first commit

---
+++ 2fa.py
@@ -0,0 +1,36 @@
+import time
+import pyotp
+import qrcode
+
+key = "RaymondTurnerMyOtp"
+
+uri = pyotp.totp.TOTP(key).provisioning_uri(name="RayTurners_2FA_App",
+                                           issuer_name="2FA App")
+
+print(uri)
+
+qrcode.make(uri).save("totp.png")
+
+
+
+
+
+
+
+# ********************
+# totp = pyotp.TOTP(key)
+# print(totp.now())
+# input_code = input("Enter 2FA Code:")
+# ********************
+
+# ********************
+# Input Code Time Tester
+# totp.verify(input_code)
+# print(totp.verify(input_code))
+# ********************
+
+# ********************
+# Time Code Reset Test 
+# time.sleep(30)
+# print(totp.now())
+# ********************
---
+++ README.md
@@ -0,0 +1,6 @@
+# Two-Factor Authentication (2FA) in Python
+
+## Libraries required
+## pip install time pytop qrcode
+
+## #rayturner.dev# two-factor-auth-2FA
